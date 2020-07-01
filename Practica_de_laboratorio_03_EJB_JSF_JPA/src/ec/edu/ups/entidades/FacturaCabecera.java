987827178d7aaac4e444babc7252a1b09167c183
Jonnathan Oswaldo Matute Curillo <34014602+Jonnathan-Matute@users.noreply.github.com>
2020-07-01 00:22:18 -0500

    Update FacturaCabecera.java

--- Practica_de_laboratorio_03_EJB_JSF_JPA/src/ec/edu/ups/entidades/FacturaCabecera.java
+++ Practica_de_laboratorio_03_EJB_JSF_JPA/src/ec/edu/ups/entidades/FacturaCabecera.java
@@ -1,8 +1,9 @@
 package ec.edu.ups.entidades;
 
 import java.io.Serializable;
-import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Objects;
 
 import javax.persistence.*;
 
@@ -14,20 +15,38 @@ import javax.persistence.*;
 
 public class FacturaCabecera implements Serializable {
 
-	
 	private static final long serialVersionUID = 1L;
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int codigo;
-	private Date fecha;
-	private double iva;
-	private double descuento;
+
+	@Temporal(TemporalType.DATE)
+	private GregorianCalendar fecha;
+	private double descuentoFact;
+	private double ivaFact;
+	private double subtotalFact;
 	private double totalFact;
-	
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "facturaCabecera")
+	private List<FacturaDetalle> listaFacturasDetalles;
+
 	@ManyToOne
 	private Usuario usuario;
-	@ManyToOne
-	private FacturaDetalle facturaDetalle;
-	
+
+	public FacturaCabecera() {
+	}
+
+	public FacturaCabecera(int codigo, GregorianCalendar fecha, double descuentoFact, double ivaFact,
+			double subtotalFact, double totalFact, List<FacturaDetalle> listaFacturasDetalles, Usuario usuario) {
+		this.codigo = codigo;
+		this.fecha = fecha;
+		this.descuentoFact = descuentoFact;
+		this.ivaFact = ivaFact;
+		this.subtotalFact = subtotalFact;
+		this.totalFact = totalFact;
+		this.listaFacturasDetalles = listaFacturasDetalles;
+		this.usuario = usuario;
+	}
 
 	public int getCodigo() {
 		return codigo;
@@ -37,146 +56,100 @@ public class FacturaCabecera implements Serializable {
 		this.codigo = codigo;
 	}
 
-
-	public Date getFecha() {
+	public GregorianCalendar getFecha() {
 		return fecha;
 	}
 
-	public void setFecha(Date fecha) {
+	public void setFecha(GregorianCalendar fecha) {
 		this.fecha = fecha;
 	}
 
-
-	public double getIva() {
-		return iva;
+	public double getDescuentoFact() {
+		return descuentoFact;
 	}
 
-	public void setIva(double iva) {
-		this.iva = iva;
+	public void setDescuentoFact(double descuentoFact) {
+		this.descuentoFact = descuentoFact;
 	}
 
-
-	public double getDescuento() {
-		return descuento;
+	public double getIvaFact() {
+		return ivaFact;
 	}
 
-
-
-	public void setDescuento(double descuento) {
-		this.descuento = descuento;
+	public void setIvaFact(double ivaFact) {
+		this.ivaFact = ivaFact;
 	}
 
+	public double getSubtotalFact() {
+		return subtotalFact;
+	}
 
+	public void setSubtotalFact(double subtotalFact) {
+		this.subtotalFact = subtotalFact;
+	}
 
 	public double getTotalFact() {
 		return totalFact;
-	}
-
-
-
-
-
-
-	public FacturaDetalle getFacturaDetalle() {
-		return facturaDetalle;
-	}
-
-	public void setFacturaDetalle(FacturaDetalle facturaDetalle) {
-		this.facturaDetalle = facturaDetalle;
 	}
 
 	public void setTotalFact(double totalFact) {
 		this.totalFact = totalFact;
 	}
 
+	public List<FacturaDetalle> getListaFacturasDetalles() {
+		return listaFacturasDetalles;
+	}
+
+	public void setListaFacturasDetalles(List<FacturaDetalle> listaFacturasDetalles) {
+		this.listaFacturasDetalles = listaFacturasDetalles;
+	}
 
 	public Usuario getUsuario() {
 		return usuario;
 	}
 
-
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
 
+	public double calcularTotal() {
+		return 0;
+	}
 
-	public FacturaCabecera() {
-		super();
+	public double calcularIvaTotal() {
+		return 0;
+	}
+
+	public double calcularSubtotal() {
+		return 0;
+	}
+
+	public double calcularDescuentoFact() {
+		return 0;
+	}
+
+	public boolean agregarDetalle(FacturaDetalle facturaDetalle) {
+		return listaFacturasDetalles.add(facturaDetalle);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof FacturaCabecera))
+			return false;
+		FacturaCabecera that = (FacturaCabecera) o;
+		return codigo == that.codigo;
 	}
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + codigo;
-		long temp;
-		temp = Double.doubleToLongBits(descuento);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		result = prime * result + ((facturaDetalle == null) ? 0 : facturaDetalle.hashCode());
-		result = prime * result + ((fecha == null) ? 0 : fecha.hashCode());
-		temp = Double.doubleToLongBits(iva);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		temp = Double.doubleToLongBits(totalFact);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		result = prime * result + ((usuario == null) ? 0 : usuario.hashCode());
-		return result;
+		return Objects.hash(codigo);
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		FacturaCabecera other = (FacturaCabecera) obj;
-		if (codigo != other.codigo)
-			return false;
-		if (Double.doubleToLongBits(descuento) != Double.doubleToLongBits(other.descuento))
-			return false;
-		if (facturaDetalle == null) {
-			if (other.facturaDetalle != null)
-				return false;
-		} else if (!facturaDetalle.equals(other.facturaDetalle))
-			return false;
-		if (fecha == null) {
-			if (other.fecha != null)
-				return false;
-		} else if (!fecha.equals(other.fecha))
-			return false;
-		if (Double.doubleToLongBits(iva) != Double.doubleToLongBits(other.iva))
-			return false;
-		if (Double.doubleToLongBits(totalFact) != Double.doubleToLongBits(other.totalFact))
-			return false;
-		if (usuario == null) {
-			if (other.usuario != null)
-				return false;
-		} else if (!usuario.equals(other.usuario))
-			return false;
-		return true;
+	public String toString() {
+		return "FacturaCabecera{" + "codigo=" + codigo + ", fecha=" + fecha + ", descuentoFact=" + descuentoFact
+				+ ", subtotalFact=" + subtotalFact + ", ivaFact=" + ivaFact + ", totalFact=" + totalFact + '}';
 	}
-
-	/**
-	 * @param codigo
-	 * @param fecha
-	 * @param iva
-	 * @param descuento
-	 * @param totalFact
-	 * @param usuario
-	 * @param facturaDetalle
-	 */
-	public FacturaCabecera(int codigo, Date fecha, double iva, double descuento, double totalFact, Usuario usuario,
-			FacturaDetalle facturaDetalle) {
-		this.codigo = codigo;
-		this.fecha = fecha;
-		this.iva = iva;
-		this.descuento = descuento;
-		this.totalFact = totalFact;
-		this.usuario = usuario;
-		this.facturaDetalle = facturaDetalle;
-	}
-
-
-	
 }
