576ba9f9931205bf5574c5f26856a63b40587e6f
Jonnathan-Matute <jmatutec2@gmail.com>
2020-07-21 00:33:59 -0500

    Creacion de los metodos de facturacion
    Creacion de los metodos de facturacion

--- Practica_de_laboratorio_03_EJB_JSF_JPA/src/ec/edu/ups/ejb/FacturaCabeceraFacade.java
+++ Practica_de_laboratorio_03_EJB_JSF_JPA/src/ec/edu/ups/ejb/FacturaCabeceraFacade.java
@@ -3,22 +3,35 @@ package ec.edu.ups.ejb;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import ec.edu.ups.entidades.FacturaCabecera;
+import ec.edu.ups.entidades.*;
 
 @Stateless
-public class FacturaCabeceraFacade extends AbstractFacade<FacturaCabecera>{
-	
+public class FacturaCabeceraFacade extends AbstractFacade<FacturaCabecera> {
+
 	@PersistenceContext(unitName = "Practica_de_laboratorio_03_EJB_JSF_JPA")
-    private EntityManager entityManager;
+	private EntityManager entityManager;
 
-    public FacturaCabeceraFacade(){
-        super(FacturaCabecera.class);
-    }
+	public FacturaCabeceraFacade() {
+		super(FacturaCabecera.class);
+	}
 
-    @Override
-    protected  EntityManager getEntityManager(){
-        return entityManager;
-    }
+	public FacturaCabecera getPedidoFacturaCabecera(Pedido pedido) {
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<FacturaCabecera> criteriaQuery = criteriaBuilder.createQuery(FacturaCabecera.class);
+		Root<FacturaCabecera> FacturaCabeceraRoot = criteriaQuery.from(FacturaCabecera.class);
+		Predicate predicatePersona = criteriaBuilder.equal(FacturaCabeceraRoot.get("pedido"), pedido);
+		criteriaQuery.select(FacturaCabeceraRoot).where(predicatePersona);
+		return entityManager.createQuery(criteriaQuery).getSingleResult();
+	}
 
+	@Override
+	protected EntityManager getEntityManager() {
+		return entityManager;
+	}
 }
--- Practica_de_laboratorio_03_EJB_JSF_JPA/src/ec/edu/ups/ejb/ProductFacade.java
+++ Practica_de_laboratorio_03_EJB_JSF_JPA/src/ec/edu/ups/ejb/ProductFacade.java
@@ -2,23 +2,130 @@ package ec.edu.ups.ejb;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NamedQuery;
 import javax.persistence.PersistenceContext;
-
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.*;
+import java.util.stream.Collectors;
+import static java.lang.String.valueOf;
+import ec.edu.ups.entidades.Category;
 import ec.edu.ups.entidades.Product;
 
 @Stateless
 public class ProductFacade extends AbstractFacade<Product>{
 
     @PersistenceContext(unitName = "Practica_de_laboratorio_03_EJB_JSF_JPA")
-    private EntityManager em;
+    private EntityManager entityManager;
     
-    public ProductFacade() {
-	super(Product.class);
-    }    
+    public ProductFacade(){
+        super(Product.class);
+        this.entityManager = this.entityManager;
+    }   
+
+    public Product buscarProducto(String nombre){
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Product> criteriaQuery = criteriaBuilder.createQuery(Product.class);
+        Root<Product> usuarioRoot=criteriaQuery.from(Product.class);
+        Predicate predicate = criteriaBuilder.equal(usuarioRoot.get("nombre"),nombre);
+        criteriaQuery.select(usuarioRoot).where(predicate);
+        return entityManager.createQuery(criteriaQuery).getSingleResult();
+    }
+
+    public Product buscarPrductoPorNombre(String nombre){
+        CriteriaBuilder criteriaBuilder= entityManager.getCriteriaBuilder();
+        CriteriaQuery<Product> criteriaQuery= criteriaBuilder.createQuery(Product.class);
+        Root<Product> categoriaRoot= criteriaQuery.from(Product.class);
+        Predicate predicate= criteriaBuilder.equal(categoriaRoot.get("nombre"),nombre);
+        criteriaQuery.select(categoriaRoot).where(predicate);
+
+        return entityManager.createQuery(criteriaQuery).getSingleResult();
+    }
+    public Product buscarProductoPorCodigo(String id){
+        CriteriaBuilder criteriaBuilder= entityManager.getCriteriaBuilder();
+        CriteriaQuery<Product> criteriaQuery= criteriaBuilder.createQuery(Product.class);
+        Root<Product> categoriaRoot= criteriaQuery.from(Product.class);
+        Predicate predicate= criteriaBuilder.equal(categoriaRoot.get("codigo"),id);
+        criteriaQuery.select(categoriaRoot).where(predicate);
+        return entityManager.createQuery(criteriaQuery).getSingleResult();
+    }
 
     @Override
-    protected EntityManager getEntityManager() {
-        return em;
+    protected EntityManager getEntityManager(){
+        return entityManager;
     }
+
+    public Map<String, String> getProductosPorCategoria(Category categoria){
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Product> productoCriteriaQuery = criteriaBuilder.createQuery(Product.class);
+        Root<Product> productoRoot = productoCriteriaQuery.from(Product.class);
+        productoCriteriaQuery.select(productoRoot)
+                .where(
+                        criteriaBuilder.equal(productoRoot.get("categoria"), categoria));
+
+         return entityManager.createQuery(productoCriteriaQuery).getResultList()
+                 .parallelStream()
+                 .collect(Collectors.toMap(Product::getCodigo, Product::getNombre)).entrySet()
+                 .parallelStream()
+                 .collect(Collectors.toMap(entry -> valueOf(entry.getKey()), Map.Entry::getValue));
+    }
+    private List<Integer> codigoProductos;
+    public List<Integer> getProductosPorBodega(int codigoBodega){
+        codigoProductos = new ArrayList<>();
+        Query query = entityManager.createNativeQuery("SELECT p FROM Producto p WHERE p.codigo =" + codigoBodega);
+        List objectList = query.getResultList();
+        if(!objectList.isEmpty()){
+            objectList.forEach(e-> {
+                codigoProductos.add(Integer.valueOf(valueOf(e)));
+            });
+        }
+        return codigoProductos;
+    }
+
+    private List<Product> productos;
+    public List<Product> getProductos(int codigoBodega, int codigoCategoria){
+        productos = new ArrayList<>();
+        List<Integer> codigosProducto = getProductosPorBodega(codigoBodega);
+        if(!codigosProducto.isEmpty()) {
+            codigosProducto.forEach(e -> productos.add(super.find(e)));
+            productos = productos
+                    .parallelStream()
+                    .filter(producto -> producto.getCategoria().getCodigo() == codigoCategoria)
+                    .collect(Collectors.toList());
+            List<Product> productoList = new ArrayList<>();
+
+            productos.forEach(p -> {
+                Product producto = new Product(p.getNombre(), p.getCostoUnitario(),
+                        p.getCantidadStock());
+                productoList.add(producto);
+            });
+
+            return productoList;
+        }
+        return productos;
+    }
+
+    private List<Category> categoriasList;
+
+    public List<Category> getCategorias(int codigoBodega){
+        categoriasList = new ArrayList<>();
+        List<Integer> codigosProductos = getProductosPorBodega(codigoBodega);
+
+        if(!codigosProductos.isEmpty()){
+            codigosProductos.forEach(e->{
+                categoriasList.add(super.find(e).getCategoria());
+            });
+            Set<Category> categorias = new HashSet<>(categoriasList);
+            categoriasList.clear();
+            categorias.forEach(e->{e.setProductos(null); categoriasList.add(e);});
+            return categoriasList;
+        }else
+            return new ArrayList<>();
+    }
+
 
 }
